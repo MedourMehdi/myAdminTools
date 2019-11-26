5549980b35801dd113e49f2104b4cb6ed60bb2e3
Administrator <mmedour@companeo.com>
2019-11-26 11:57:00 +0100

    Fixes

--- Bash_DevOps/Merge_Web_Log.sh
+++ Bash_DevOps/Merge_Web_Log.sh
@@ -6,8 +6,15 @@
 SITENAME=$1
 CONFNAME=`echo "${SITENAME}" |  sed 's/www.//'`
 WORKDIR="/var/log/apache2"
+SRC_SRV_LIST="web1 web2 web3"
+SRC_APACHE_LOG="/var/log/apache2"
 MAIL_ADMIN=seo@yourdomain.com
 REMOTE_WEB_USER=apacheuser
+SSH_KEY_PATH="/var/backups/.ssh/id_rsa_X"
+MERGE_SCRIPT="/usr/share/awstats/tools/logresolvemerge.pl"
+TMP_WORK_DIR="~/merge"
+DEST_DIR="~/merge/all_days"
+NFS_BACKUP_DIR="~/logs_web_production/web_logs"
 
 #
 #   Active or not SEO suppliers upload
@@ -26,25 +33,25 @@ ThirdLevelNDD="${SITENAME##*.}"
 #   Rsync log and log.1 for each web server
 #
 echo "DIFF log pour ${SITENAME}"
-for s in web1 web2 web3 ; do
-rsync -avz  -e "ssh -i /var/backups/.ssh/id_rsa_amdump_new -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" ${REMOTE_WEB_USER}@$s:/var/log/apache2/access-${SITENAME}.log /var/log/apache2/$s/
-rsync -avz -e "ssh -i /var/backups/.ssh/id_rsa_amdump_new -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" ${REMOTE_WEB_USER}@$s:/var/log/apache2/access-${SITENAME}.log.1 /var/log/apache2/$s/
+for s in ${SRC_SRV_LIST} ; do
+rsync -avz  -e "ssh -i ${SSH_KEY_PATH} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" ${REMOTE_WEB_USER}@$s:${SRC_APACHE_LOG}/access-${SITENAME}.log ${WORKDIR}/$s/
+rsync -avz -e "ssh -i ${SSH_KEY_PATH} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" ${REMOTE_WEB_USER}@$s:${SRC_APACHE_LOG}/access-${SITENAME}.log.1 ${WORKDIR}/$s/
 done
 
 #
 #   Merging three apache logs into one.
-#   Dest file is ~/merge/access-${SITENAME}.log.temp
+#   Dest file is ${TMP_WORK_DIR}/access-${SITENAME}.log.temp
 #
 for f in access-${SITENAME}.log.1 access-${SITENAME}.log;do
-    /usr/share/awstats/tools/logresolvemerge.pl ${WORKDIR}/web1/$f ${WORKDIR}/web2/$f ${WORKDIR}/web3/$f;
-done > ~/merge/access-${SITENAME}.log.temp
+    ${MERGE_SCRIPT} ${WORKDIR}/web1/$f ${WORKDIR}/web2/$f ${WORKDIR}/web3/$f;
+done > ${TMP_WORK_DIR}/access-${SITENAME}.log.temp
 
 #
 #   This file is created below and contain the last date and hour processed
 #   If file is not present we choose an arbitrary date (Yesterday)
 #
-if [ -f  ~/merge/all_days/access-${SITENAME}.last.log ];then
-                LASTDATE=`cat ~/merge/all_days/access-${SITENAME}.last.log`;
+if [ -f  ${DEST_DIR}/access-${SITENAME}.last.log ];then
+                LASTDATE=`cat ${DEST_DIR}/access-${SITENAME}.last.log`;
 [ ".${LASTDATE}" == "." ] && LASTDATE=`LC_ALL=en_GB.UTF-8 date --d="-1 day" +"%d/%b/%Y"`
 fi;
 
@@ -60,14 +67,14 @@ echo "${LASTDATE_2}"
 #
 #   Truncate Apache Log begining from LASTDATE_2
 #
-`sed -n '/'"${LASTDATE_2}"'/,$'p ~/merge/access-${SITENAME}.log.temp > ~/merge/all_days/access-${SITENAME}.log`
+`sed -n '/'"${LASTDATE_2}"'/,$'p ${TMP_WORK_DIR}/access-${SITENAME}.log.temp > ${DEST_DIR}/access-${SITENAME}.log`
 
 #
 #   Getting date interval information about logs merging
 #   Used to store log archive with comprhensive date in filename
 #   Also used to create the LASTDATE file.
 #
-`tail -n 1 ~/merge/all_days/access-${SITENAME}.log > endfile.temp`;
+`tail -n 1 ${DEST_DIR}/access-${SITENAME}.log > endfile.temp`;
 date=`awk -F " " '{print $4}' endfile.temp | tr "[" " "`;
 
 echo $date > datefile.temp;
@@ -81,9 +88,9 @@ DATEDEBUT=`date -d "${day} ${month} ${year}" +"%Y%m%d"`;
 
 echo "DATEDEBUT equal ${DATEDEBUT}"
 
-cp datefile.temp  ~/merge/all_days/access-${SITENAME}.last.log
+cp datefile.temp  ${DEST_DIR}/access-${SITENAME}.last.log
 
-`head -1 ~/merge/all_days/access-${SITENAME}.log > startfile.temp`;
+`head -1 ${DEST_DIR}/access-${SITENAME}.log > startfile.temp`;
 date=`awk -F " " '{print $4}' startfile.temp | tr "[" " "`;echo $date > datefile.temp;
 day=`awk -F "/" '{print $1}' datefile.temp`;
 month=`awk -F "/" '{print $2}' datefile.temp`;
@@ -95,21 +102,17 @@ echo "DATEFIN equal ${DATEFIN}"
 
 rm year.temp;rm datefile.temp;rm startfile.temp;rm endfile.temp;
 
-`mv ~/merge/all_days/access-${SITENAME}.log ~/merge/all_days/access-${SITENAME}.${DATEFIN}-${DATEDEBUT}.log`
-
-#if [ ${LogStash_Active} -gt 0 ];then
-#       $(cp ~/merge/all_days/access-${SITENAME}.${DATEFIN}-${DATEDEBUT}.log /var/backups/merge/logstash/)
-#fi
+`mv ${DEST_DIR}/access-${SITENAME}.log ${DEST_DIR}/access-${SITENAME}.${DATEFIN}-${DATEDEBUT}.log`
 
 #
 #   Archiving
 #
-`gzip --force ~/merge/all_days/access-${SITENAME}.${DATEFIN}-${DATEDEBUT}.log`
+`gzip --force ${DEST_DIR}/access-${SITENAME}.${DATEFIN}-${DATEDEBUT}.log`
 
 #
 #   Store in safe place
 #
-`cp -a ~/merge/all_days/access-${SITENAME}.${DATEFIN}-${DATEDEBUT}.log.gz ~/logs_web_production/web_logs/access-${SITENAME}.${DATEFIN}-${DATEDEBUT}.log.gz`
+`cp -a ${DEST_DIR}/access-${SITENAME}.${DATEFIN}-${DATEDEBUT}.log.gz ${NFS_BACKUP_DIR}/access-${SITENAME}.${DATEFIN}-${DATEDEBUT}.log.gz`
 
 echo "Fin ${SITENAME}"
 
@@ -121,7 +124,7 @@ PASSWD="my_pass"
 HOST="ftp.wedoseo4you.com"
 
 if [ ${FTP_Upload_Active} -gt 0 ];then
-        FTPSTATUS=`curl  -s -w '%{http_code}' -u ${USER}:${PASSWD} -T ~/merge/all_days/access-${SITENAME}.${DATEFIN}-${DATEDEBUT}.log.gz ftp://${HOST}/`
+        FTPSTATUS=`curl  -s -w '%{http_code}' -u ${USER}:${PASSWD} -T ${DEST_DIR}/access-${SITENAME}.${DATEFIN}-${DATEDEBUT}.log.gz ftp://${HOST}/`
         if [ "${FTPSTATUS}" = "226"  ]; then
                 DATE_SEND=`date +"%Y%m%d%H"`
                 echo "${DATE_SEND} access-${SITENAME}.${DATEFIN}-${DATEDEBUT}.log.gz done. FTP Status ${FTPSTATUS}" >> /tmp/log_FTP_Upload_OK_ftp.log
